db6512e038d388ee3a4ae3757acd19e1e4aa3a25
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-08-07 14:31:46 +0545

    30 days first push

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app2 = express()
+
+const PORT =  3001
+const cors = require('cors')
+app2.use(cors())
+
+
+
+
+app2.get('/route', (req, res) =>{
+  res.status(200)
+  res.send({message:"Hello, World!"})
+})
+
+app2.listen(PORT, error => {
+  if(!error)console.log('Server running on port',PORT)
+  else console.log(error)
+})
---
+++ backend/src/Routes/userRouter.js
@@ -0,0 +1,11 @@
+// import { Router } from "express";
+
+// let userRouter = Router();
+
+// userRouter
+//   .route("/:id")
+//   .get(readUserDetails)
+//   .patch(updateUser)
+//   // .delete(isAuthenticated,isAuthorized,deleteUser);
+
+// export default userRouter;
